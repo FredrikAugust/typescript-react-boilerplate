168d709ca1be24b7e3a9bef99e99a533c6d4fb66
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2018-02-02 00:47:06 +0100

    Change to dev build of react, remove dev-server

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,8 +9,8 @@ export class App extends React.Component<AppProps, {}> {
   render() {
     return (
       <div>
-        <h1>Hello, World.</h1>
-        <p>This website is made with { this.props.framework } and compiled with { this.props.compiler }.</p>
+        <h1>OK.</h1>
+        <p>This website is made with { this.props.framework }, and has been compiled with { this.props.compiler }.</p>
       </div>
     );
   }
